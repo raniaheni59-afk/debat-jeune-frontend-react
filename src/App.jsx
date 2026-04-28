0ad6831cb14f6ddc7273101c6835f81d3108e410
Rania <raniaheni59@gmail.com>
2026-04-28 15:55:23 +0200

    fix: save all changes

--- src/App.jsx
+++ src/App.jsx
@@ -69,6 +69,7 @@ const ProtectedAdminRoute = ({ children }) => {
           <AdminDashboard />
         </ProtectedAdminRoute>
       } />
+      <Route path="/messenger" element={<MessengerPage />} />
       <Route
   path="/messenger"
   element={
