ba1b60065f28912fef607e8465f6fe4421f9718f
Rania <raniaheni59@gmail.com>
2026-04-29 22:47:10 +0200

    fix: add Routes to react-router-dom import

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-        <Route path="/profile/:id" element={<ProfilePage />} />
-        <Route path="*" element={<Navigate to="/profile/1" />} />
-      </Routes>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
